59c65bf9f1ed747cd7a88c77e837c6e82c0f06f5
Sherwin688 <sherwin.dcosta688@gmail.com>
2023-05-15 15:44:13 +0530

    added add additional task and auto add daily task when click on any date

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -40,9 +40,7 @@ function Dashboard() {
   }
  
   const handleDayClick = (val)=>{
-
     setValue(val);
-
     findTask(val)
     
   }
--- src/components/DateModal.jsx
+++ src/components/DateModal.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal';
 import Task from "./Task";
 import axios from "axios";
 import { formatDate } from "react-calendar/dist/cjs/shared/dateFormatter";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {v4 as uuid} from "uuid"; 
 
 function DateModal({dateModalIsOpen,setDateModalIsOpen,datetasks,date}) {
   const [tasks,setTasks] = useState(datetasks)
   const [value,setValue] = useState("")
   const [addModalIsOpen,setAddModalIsOpen] = useState(false)
 
-  console.log(datetasks[datetasks.length])
-  const addAdditionalTask = (date,datetasks)=>{
-    setTasks([...datetasks,{"id":parseInt(datetasks[datetasks.length-1].id)+1,"task":value,status:"incomplete","taskType":"additional"}])
+  const addAdditionalTask = (date,datetasks,e)=>{
+    const currentTaskId = (datetasks.length<1)?1:parseInt(datetasks[datetasks.length-1].id)+1
+
     axios.put("http://localhost:8000/update",{
       "date":date===""?"":formatDate("en-GB",date),
-      "tasks":[...datetasks,{"id":parseInt(datetasks[datetasks.length-1].id)+1,"task":value,status:"incomplete","taskType":"additional"}]
+      "tasks":[...datetasks,{"id":currentTaskId,"task":value,status:"incomplete","taskType":"additional"}]
     }).then((response)=>{
       console.log(response);
       setValue("")
+      setTasks(response.data.data.tasks)
       setAddModalIsOpen(false)
-      // setTasks([...datetasks,{"task":e.target.value,status:"incomplete","type":"additional"}])
     })
   }
 const updateTask = (date,datetasks)=>{
@@ -34,7 +33,7 @@ const updateTask = (date,datetasks)=>{
   })
 }
   const handleCheckboxChange = (id,e)=>{
-    tasks.map((task)=>{
+    tasks.forEach((task)=>{
       if(task.id===id){
         if(e.target.checked){
           task.status="complete"
@@ -49,7 +48,6 @@ const updateTask = (date,datetasks)=>{
     setTasks(tasks)
       updateTask(date,tasks)
   }
-  console.log(tasks.length);
   return (
     <>
   
@@ -67,7 +65,7 @@ const updateTask = (date,datetasks)=>{
         {tasks.length>0?tasks.map((task)=>task.taskType==="additional"?<Task handleCheckboxChange={handleCheckboxChange} key={task.id} id={task.id} task={task.task} status={task.status}/>:""):"No Tasks for the day"}
         {addModalIsOpen?
         <><Form.Control value={value} onChange={(e)=>setValue(e.target.value)} type="text" placeholder="Enter Task" className="mb-2" />
-        <Button onClick={()=>addAdditionalTask(date,datetasks)} variant="primary">Add</Button></>
+        <Button onClick={()=>addAdditionalTask(date,tasks)} variant="primary">Add</Button></>
         :<div className="btn-container">
         <Button onClick={()=>setAddModalIsOpen(true)} variant="primary">Add Task</Button>
         </div>}
