d6046aeb215feacc39f86d9dadd4a53db84c0752
Sherwin688 <sherwin.dcosta688@gmail.com>
2023-06-04 21:41:19 +0530

    date modal bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
+import { useEffect } from "react";
 import './App.css';
 import Dashboard from "./components/Dashboard";
-// import Calendar from "./components/Calendar";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import {Routes,Route} from "react-router-dom"
-// require('dotenv').config()
 
 function App() {
+  
+
+  useEffect(() => {
+    document.title = 'Productivity Calendar';
+  }, []);
+
   return (
     <div className="App">
      <NavBar/>
@@ -15,6 +20,7 @@ function App() {
       <Route path="/" element={<Dashboard/>}/>
       <Route path="dashboard" element={<Dashboard/>}/>
       <Route path="/today" element={<Home/>}/>
+      <Route path="*" element={<Dashboard/>}/>
      </Routes>
     </div>
   );
